1611ea107a7d47e8266cb3d39e7767cdaf6690f7
iulianab <iuliana@cs.ubbcluj.ro>
2020-03-09 15:18:12 +0200

    Modified the file to exclude single letters from the feature list.

--- representation/TfIdfStrategy.py
+++ representation/TfIdfStrategy.py
@@ -16,6 +16,8 @@ class TfIdfStrategy(RepresentationStrategy.RepresentationStrategy):
     def transform(self, seq):
         rez = []
         all_combinations = ExtractFeatureStrategy.generate_possible_sequences(3)
+        # remove single letters from the list (they do not bring relevant information in the context of tf-idf, as they occur in all sequences)
+        all_combinations = all_combinations[4:]
         features = get_tf_idf_for_sequence(seq, self._sequences)
         for i in range(0, len(all_combinations)):
             rez.append(features[all_combinations[i]])
@@ -33,15 +35,15 @@ def count_sequences_containing_subsequence(subsequence, sequences):
 
 def get_tf_idf_for_sequence(dna_seq, sequences):
     seq_list = ExtractFeatureStrategy.generate_possible_sequences(3)
+    # remove single letters from the list (they do not bring relevant information in the context of tf-idf, as they occur in all sequences)
+    seq_list = seq_list[4:]
     seq = Seq(dna_seq)
     feature_map = {}
 
     for combination in seq_list:
         # We will count the overlapping values and divide them according to their length
         divide_number = 1
-        if len(combination) == 1:
-            divide_number = len(dna_seq)
-        elif len(combination) == 2:
+        if len(combination) == 2:
             divide_number = len(dna_seq) - 1
         elif len(combination) == 3:
             divide_number = len(dna_seq) - 2
