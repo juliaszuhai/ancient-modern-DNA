829116cb455b4ff7c0b1a0566619145e7383d463
Iuliana Bocicor <iuliana@cs.ubbcluj.ro>
2020-03-22 16:47:36 +0200

    Modifications for the optimisation of fature computing.

--- representation/TfIdfStrategy.py
+++ representation/TfIdfStrategy.py
@@ -1,6 +1,5 @@
-from Bio.Seq import Seq
-import itertools
 import math
+from Bio.Seq import Seq
 from representation import ExtractFeatureStrategy
 from representation import RepresentationStrategy
 import numpy as np
@@ -18,13 +17,16 @@ class TfIdfStrategy(RepresentationStrategy.RepresentationStrategy):
         all_combinations = ExtractFeatureStrategy.generate_possible_sequences(3)
         # remove single letters from the list (they do not bring relevant information in the context of tf-idf, as they occur in all sequences)
         all_combinations = all_combinations[4:]
-        features = get_tf_idf_for_sequence(seq, self._sequences)
-        for i in range(0, len(all_combinations)):
-            rez.append(features[all_combinations[i]])
+        idf_map = get_idf_for_subsequences(all_combinations, self._sequences)
+        features = get_tf_idf_for_sequence(seq, idf_map, len(self._sequences))
+        rez = list(features.values())
 
         return np.array(rez)
 
 def count_sequences_containing_subsequence(subsequence, sequences):
+    """
+    From a list of sequences (e.g. an entire corpus), it counts how many times the subsequence occurs.
+    """
     result = 0
     for s in sequences:
         seq = Seq(s)
@@ -33,7 +35,27 @@ def count_sequences_containing_subsequence(subsequence, sequences):
             result += 1
     return result
 
-def get_tf_idf_for_sequence(dna_seq, sequences):
+def get_idf_for_subsequences(list_of_subsequences, sequences):
+    """
+        For a list of given subsequences (n-grams of leters), it builds a dictionary which memorizes how many sequences
+        (documents) contain each given subsequence
+    """
+    result = {}
+    i = 0
+    for subsequence in list_of_subsequences:
+        result[subsequence] = count_sequences_containing_subsequence(subsequence, sequences)
+        i += 1
+        print("Subsequence: " + subsequence)
+    return result
+
+def get_tf_idf_for_sequence(dna_seq, idf_map, size):
+    """
+    :param dna_seq: the current sequence
+    :param idf_map: a dictionary which was previously populated, containing the IDF for each subsequence (combination: AA, AC, ... TTT),
+                    computed for the entire corpus
+    :param size: the total number of sequences
+    :return:
+    """
     seq_list = ExtractFeatureStrategy.generate_possible_sequences(3)
     # remove single letters from the list (they do not bring relevant information in the context of tf-idf, as they occur in all sequences)
     seq_list = seq_list[4:]
@@ -48,11 +70,9 @@ def get_tf_idf_for_sequence(dna_seq, sequences):
         elif len(combination) == 3:
             divide_number = len(dna_seq) - 2
         tf = seq.count_overlap(combination) / divide_number
-        count = count_sequences_containing_subsequence(combination, sequences)
-        if count == 0:
-            idf = 0
-        else:
-            idf = math.log2(len(sequences) / count)
+        idf = idf_map[combination]
+        if idf != 0:
+            idf = math.log2(size / idf)
         feature_map[combination] = tf * idf
 
     return feature_map
@@ -61,4 +81,4 @@ if __name__ == "__main__":
     sequences = ["ACGGTAACGGTG", "TTGCCTGTGCATGA", "ACCGGTTCAACGTGCAAAACGCGCACCGC"]
     tf_idf = TfIdfStrategy(sequences)
     array = tf_idf.transform(sequences[0])
-    print(array.shape)
+    print(array)
