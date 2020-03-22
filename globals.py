95f8244b576946e185bf586573c66ccf3a60b8f6
Iuliana Bocicor <iuliana@cs.ubbcluj.ro>
2020-03-22 16:47:54 +0200

    Global paths.

---
+++ globals.py
@@ -0,0 +1,7 @@
+"""
+    This file will contain global paths needed for accessing the data.
+    Processed data is very large and will not be pushed to Git.
+    The "data" folder is shared on Google Drive.
+"""
+
+PROCESSED_DATA_FOLDER = "c:/Google Drive/Universitate/Research/Bioinformatics/Ancient DNA/Ancient DNA Analysis/data"
